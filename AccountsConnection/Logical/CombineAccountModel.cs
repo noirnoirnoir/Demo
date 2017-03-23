ad2bf950a85afb16b8ce1f1edd9881de22a94bdd
noirnoirnoir <daniellewu99@gmail.com>
2017-03-23 18:21:36 +0800

    Create CombineAccountModel.cs

---
+++ AccountsConnection/Logical/CombineAccountModel.cs
@@ -0,0 +1,42 @@
+using LitangOfficial.Backoffice.BusinessLogic.Model;
+using System;
+using System.Collections.Generic;
+
+namespace LitangOfficial.Backoffice.BusinessLogic.ViewModel
+{
+    public class CombineAccountModel
+    {
+        ///EX:wife ID
+        public Guid Id { get; set; }
+
+        ///EX:wife
+        public Guid UserId { get; set; }
+
+        ///EX:husband 
+        public Guid CombinedId { get; set; }
+        public string UserCode { get; set; }
+        public string CombinedCode { get; set; }
+        public string UserName { get; set; }
+        public string CombinedName { get; set; }
+
+        public string Email { get; set; }
+        public string CellPhone { get; set; }
+        public FormStatus Status { get; set; }
+        public string File { get; set; }
+        public string FileName { get; set; }
+
+        /// T:upward, F:downward
+        public bool Position { get; set; }
+        public DateTime CreatedTime { get; set; }
+        public Guid ModifiedBy { get; set; }
+        public DateTime ModifiedTime { get; set; }             
+        public string ModifiedName { get; set; }
+        public string ModifiedDateTime { get; set; }
+
+        public List<OrganizationModel> Organizations { get; set; }
+
+
+
+
+}
+}
